2178e9ba5be1851258bc4a40d9dd325ef3f63a71
Joseph <joseph.larralde@gmail.com>
2018-06-18 16:59:09 +0200

    updated scripts to generate a folder for deken

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+# this creates a jl directory for publication with deken :
+
+JLDIR=../jl
+# JLDIR=/Users/larralde/Documents/Pd/externals/jl
+
+rm -rf $JLDIR;
+
 make clean;
 make install;
-/Applications/Pd-0.48-1-i386.app/Contents/Resources/bin/pd -open "/Users/larralde/Desktop/pd-gbend-testing.pd" &
+make clean;
+
+cp ../abstractions/* $JLDIR;
+cp ../helpfiles/* $JLDIR;
+
+mkdir $JLDIR"/src";
+cp -r ./* $JLDIR"/src";
+
+# or
+# modify the makefile to install binaries in ~/Documents/Pd/externals, switch JLDIR
+# and comment the lines that copy the sources
+
+# then you can run some test patch after build :
+# /Applications/Pd-0.48-1-i386.app/Contents/Resources/bin/pd -open "/Users/larralde/Desktop/pd-gbend-testing.pd" &
